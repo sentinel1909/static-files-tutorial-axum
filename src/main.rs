90317e0ac14efbfd3b63b236994a8d97035c37c3
Jeff Mitchell <sentinel1909@protonmail.com>
2023-06-05 06:17:37 -0700

    quick update

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,16 @@
 // src/main.rs
 
-// dependencies
 use axum::Router;
 use std::path::PathBuf;
 use tower_http::services::ServeDir;
 
-// main function, annotated for the shuttle_runtime as well as shuttle_static_folder
 #[shuttle_runtime::main]
 async fn axum(
     #[shuttle_static_folder::StaticFolder] static_folder: PathBuf,
 ) -> shuttle_axum::ShuttleAxum {
-    // build the router
+    
     let router = Router::new()
-        // serve the web files up at the "/" route
+        
         .nest_service("/", ServeDir::new(static_folder));
 
     Ok(router.into())
